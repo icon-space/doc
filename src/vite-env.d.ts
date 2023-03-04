661f3dbd63783074f28d8a70c6e481befc639454
lhlyu <misakafs@gmail.com>
2023-03-04 14:29:23 +0800

    update ci

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,9 @@
 /// <reference types="vite/client" />
 
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
+
 declare module 'colorpicker-v3' {
     import ColorPicker from 'colorpicker-v3'
     export default ColorPicker
--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,33 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ArcoResolver } from 'unplugin-vue-components/resolvers'
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-        AutoImport({
-            resolvers: [ArcoResolver()]
-        }),
-        Components({
-            resolvers: [
-                ArcoResolver({
-                    sideEffect: true
-                })
-            ]
-        })
-    ],
-    build: {
-        target: 'es2015',
-        cssTarget: 'chrome61',
+
+export default defineConfig(({ mode }) => {
+    const base = mode === 'github' ? '/doc/' : '/'
+    return {
+        base: base,
+        plugins: [
+            vue(),
+            AutoImport({
+                resolvers: [ArcoResolver()]
+            }),
+            Components({
+                resolvers: [
+                    ArcoResolver({
+                        sideEffect: true
+                    })
+                ]
+            }),
+            chunkSplitPlugin({
+                strategy: 'default'
+            })
+        ],
+        build: {
+            target: 'es2015',
+            cssTarget: 'chrome61',
+        }
     }
 })
