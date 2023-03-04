db1d94807b778d2ea735644d69c82a0d473c79e6
lhlyu <misakafs@gmail.com>
2023-03-04 14:46:38 +0800

    update route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from './routes'
 
+const base = import.meta.env.MODE == 'github' ? '/doc/' : '/'
+
 const router = createRouter({
-    history: createWebHistory('/'),
+    history: createWebHistory(base),
     scrollBehavior() {
         // 始终滚动到顶部
         return { top: 0, behavior: 'smooth' }
